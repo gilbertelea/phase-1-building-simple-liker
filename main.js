755aff7e97daf511ed149af7ee14bf6a087bee8b
gilbert <eleazarbiboum@gmail.com>
2022-05-10 10:07:54 -0400

    Assignment completed

--- main.js
+++ main.js
@@ -3,13 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+.then(()=>{
+  if ( heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART;
+    heart.style.color = "red";
+    heart.className = "activated-heart";
+  } else {
+      heart.innerText = EMPTY_HEART;
+      heart.style.color = "";
+      heart.className = "";
+  }
+})
+.catch((error)=>{
+  const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+});
+
+for (const glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
 
 
-
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
@@ -20,6 +37,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
-  });
+    }, 300)
+  })
+
 }
